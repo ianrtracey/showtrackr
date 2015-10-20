69a5fb0a99b56681489fb43aea6201d16494ad3a
Ian Tracey <ianrtracey@gmail.com>
2015-10-20 12:49:15 -0700

    Adds dependency injection variable name in order to future-proof against minification

--- showtrackr/app.js
+++ showtrackr/app.js
@@ -1,4 +1,6 @@
 angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute','mgcrea.ngStrap'])
-	.config(function () {
-
-	});
+	// We can annotate this depedency injection so that uglify
+	// and minify do not butcher the variable name
+	.config(['$locationProvider', function($locationProvider) {
+		$locationProvider.hmtl5Mode(true);	
+	}]);
